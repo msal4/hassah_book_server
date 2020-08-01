e030cde3f63bd3f874011d5c2f70d5eeb2c160c8
Mohammed Salman <msal4@outlook.com>
2020-08-02 00:50:26 +0300

    add typedi's Contianer to buildSchema to inject repositories in resolvers

--- api/src/utils/createSchema.ts
+++ api/src/utils/createSchema.ts
@@ -1,8 +1,10 @@
 import { buildSchema } from "type-graphql";
 import { join } from "path";
+import { Container } from "typedi";
 
 export const createSchema = () => {
   return buildSchema({
     resolvers: [join(__dirname, "../modules/**/*.resolver.ts")],
+    container: Container,
   });
 };
