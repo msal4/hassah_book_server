053f8c502537745fcb9fdc9e3c9abe8f4b1d50e8
Mohammed Salman <msal4@outlook.com>
2020-08-01 23:50:12 +0300

    extract refreshToken to its own function

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,17 +5,11 @@ import { ApolloServer } from "apollo-server-express";
 import { createConnection } from "typeorm";
 import express from "express";
 import cookieParser from "cookie-parser";
-import { verify } from "jsonwebtoken";
 
-import {
-  getRefreshSecret,
-  createAccessToken,
-  sendRefreshTokenCookie,
-  createRefreshToken,
-} from "@api/modules/user/auth";
-import { User } from "@api/entity/User";
-import { JwtRefreshPayload } from "@api/modules/types/JwtPayload";
 import { createSchema } from "@api/utils/createSchema";
+import { refreshToken } from "./utils/refreshToken";
+
+const PORT = 4000;
 
 const main = async () => {
   await createConnection();
@@ -23,33 +17,10 @@ const main = async () => {
 
   const app = express();
   app.use(cookieParser());
-  app.get("/", (_req, res) => res.send("hi"));
-  app.post("/refresh_token", async (req, res) => {
-    const token = req.cookies.skal as string;
-    if (!token) {
-      return res.send({ ok: false, accessToken: "" });
-    }
-
-    try {
-      const payload = verify(token, getRefreshSecret()) as JwtRefreshPayload;
-      const user = await User.findOne(payload?.userId);
-      if (!user) {
-        return res.send({ ok: false, accessToken: "" });
-      }
-
-      // Checks if the token is invalidated.
-      if (payload.tokenVersion !== user.tokenVersion) {
-        return res.send({ ok: false, accessToken: "" });
-      }
-
-      sendRefreshTokenCookie(res, createRefreshToken(user));
-
-      return res.send({ ok: true, accessToken: createAccessToken(user) });
-    } catch (err) {
-      console.log(err);
-      return res.send({ ok: false, accessToken: "" });
-    }
-  });
+  app.get("/", (_req, res) =>
+    res.send('<a href="/graphql">Graphql PlayGround</a>')
+  );
+  app.post("/refresh_token", refreshToken);
 
   const apolloServer = new ApolloServer({
     schema,
@@ -59,8 +30,9 @@ const main = async () => {
     }),
   });
   apolloServer.applyMiddleware({ app });
-  app.listen(4000, () => {
-    console.log("listening on port 4000");
+
+  app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
   });
 };
 
---
+++ api/src/utils/refreshToken.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+import {
+  sendRefreshTokenCookie,
+  createAccessToken,
+  createRefreshToken,
+  getRefreshSecret,
+} from "@api/modules/user/auth";
+import { JwtRefreshPayload } from "@api/modules/types/JwtPayload";
+import { User } from "@api/entity/User";
+
+export async function refreshToken(req: Request, res: Response) {
+  const token = req.cookies.skal as string;
+  if (!token) {
+    return res.send({ ok: false, accessToken: "" });
+  }
+
+  try {
+    const payload = verify(token, getRefreshSecret()) as JwtRefreshPayload;
+    const user = await User.findOne(payload?.userId);
+    if (!user) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    // Checks if the token is invalidated.
+    if (payload.tokenVersion !== user.tokenVersion) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    sendRefreshTokenCookie(res, createRefreshToken(user));
+
+    return res.send({ ok: true, accessToken: createAccessToken(user) });
+  } catch (err) {
+    console.log(err);
+    return res.send({ ok: false, accessToken: "" });
+  }
+}
