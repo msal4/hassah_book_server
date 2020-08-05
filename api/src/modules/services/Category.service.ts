a0779aaec056cfaa390a3b7266b17c732295b5ec
Mohammed Salman <msal4@outlook.com>
2020-08-05 05:19:24 +0300

    rename to BaseGroup.service

--- api/src/modules/services/Category.service.ts
+++ api/src/modules/services/Category.service.ts
@@ -1,7 +1,7 @@
 import { Service } from "typedi";
 
 import { Category } from "@api/entity/Category";
-import BaseGroupService from "./BaseGroupService.service";
+import BaseGroupService from "./BaseGroup.service";
 
 @Service()
 export class CategoryService extends BaseGroupService<Category> {
