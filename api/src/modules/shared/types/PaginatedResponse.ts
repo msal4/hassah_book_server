79b6e2a7d8d37f499f2c1f146791ef1ae0f906e9
Mohammed Salman <msal4@outlook.com>
2020-08-05 20:58:04 +0300

    create paginated response types for Author and Publisher

--- api/src/modules/shared/types/PaginatedResponse.ts
+++ api/src/modules/shared/types/PaginatedResponse.ts
@@ -4,6 +4,8 @@ import { Product } from "@api/entity/Product";
 import { Favorite } from "@api/entity/Favorite";
 import { Category } from "@api/entity/Category";
 import { Collection } from "@api/entity/Collection";
+import { Author } from "@api/entity/Author";
+import { Publisher } from "@api/entity/Publisher";
 
 export function PaginatedResponse<T>(TClass: ClassType<T>) {
   @ObjectType(`Paginated${TClass.name}Response`)
@@ -26,6 +28,12 @@ export type PaginatedCategoryResponse = InstanceType<typeof PaginatedCategoryRes
 
 export const PaginatedCollectionResponse = PaginatedResponse(Collection);
 export type PaginatedCollectionResponse = InstanceType<typeof PaginatedCollectionResponse>;
+
+export const PaginatedAuthorResponse = PaginatedResponse(Author);
+export type PaginatedAuthorResponse = InstanceType<typeof PaginatedAuthorResponse>;
+
+export const PaginatedPublisherResponse = PaginatedResponse(Publisher);
+export type PaginatedPublisherResponse = InstanceType<typeof PaginatedPublisherResponse>;
 
 export const PaginatedProductResponse = PaginatedResponse(Product);
 export type PaginatedProductResponse = InstanceType<typeof PaginatedProductResponse>;
