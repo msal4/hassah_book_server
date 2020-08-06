83080020b67cf8fc1516a82d22697b7b53db6fee
Mohammed Salman <msal4@outlook.com>
2020-08-07 01:50:21 +0300

    Validate the id

--- api/src/modules/shared/types/ObjectId.ts
+++ api/src/modules/shared/types/ObjectId.ts
@@ -1,7 +1,9 @@
 import { Field, ID, InputType } from "type-graphql";
+import { IsNotEmpty } from "class-validator";
 
 @InputType()
 export class ObjectId {
   @Field(() => ID)
+  @IsNotEmpty()
   id: string;
 }
