04a7c354bd913531e24ca8ff0c837d6182906c4d
Mohammed Salman <msal4@outlook.com>
2020-08-07 01:07:39 +0300

    Add the phone field to the order factory

--- api/src/factories/order.ts
+++ api/src/factories/order.ts
@@ -8,6 +8,7 @@ define(Order, (faker, context?: OrderFactoryContext) => {
   const order = new Order();
   order.address = faker.address.streetAddress();
   order.user = context!.user;
+  order.phone = faker.phone.phoneNumber("079########");
   order.status = faker.random.arrayElement(enumToList(OrderStatus));
   order.createdAt = faker.date.past();
   return order;
