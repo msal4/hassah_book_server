9f00fdf561881452de5460aab0cb741c282e1a2b
Mohammed Salman <msal4@outlook.com>
2020-08-07 22:32:12 +0300

    Remove IsEmailAlreadyExist validator

--- api/src/modules/admin/admin/AdminLoginInput.ts
+++ api/src/modules/admin/admin/AdminLoginInput.ts
@@ -2,7 +2,6 @@ import { InputType, Field } from "type-graphql";
 import { IsNotEmpty, IsEmail } from "class-validator";
 
 import { Admin } from "@api/entity/Admin";
-import { IsAdminEmailAlreadyExist } from "@api/modules/admin/admin/IsAdminEmailAlreadyExist";
 
 @InputType()
 export class AdminLoginInput implements Partial<Admin> {
@@ -13,6 +12,5 @@ export class AdminLoginInput implements Partial<Admin> {
 
   @Field()
   @IsNotEmpty()
-  @IsAdminEmailAlreadyExist()
   password: string;
 }
