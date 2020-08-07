4b55fbe85d8b61f319bd0fa675f392f60b9a2559
Mohammed Salman <msal4@outlook.com>
2020-08-07 05:59:44 +0300

    Increase the expiration period and accept any entity that extends BaseUser instead of just User

--- api/src/modules/user/auth.ts
+++ api/src/modules/user/auth.ts
@@ -1,17 +1,19 @@
 import { sign } from "jsonwebtoken";
 import { Response } from "express";
 
-import { User } from "@api/entity/User";
+import { BaseUser } from "@api/entity/shared/BaseUser";
 
+// TODO: check whether the environment is development or production and set the secret
+// based on that instead of just hardcoding it.
 export const getAccessSecret = () => process.env.JWT_ACCESS_SECRET ?? "testsecretkey";
 export const getRefreshSecret = () => process.env.JWT_REFRESH_SECRET ?? "anothertestsecretkey";
 
-export const createAccessToken = (user: User) =>
+export const createAccessToken = (user: BaseUser) =>
   sign({ userId: user.id }, getAccessSecret(), { expiresIn: "15m" });
 
-export const createRefreshToken = (user: User) =>
+export const createRefreshToken = (user: BaseUser) =>
   sign({ userId: user.id, tokenVersion: user.tokenVersion }, getRefreshSecret(), {
-    expiresIn: "1w",
+    expiresIn: "8w",
   });
 
 export const sendRefreshTokenCookie = (res: Response, token: string) => {
