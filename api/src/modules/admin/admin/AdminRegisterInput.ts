bfbfa902c8cb5303b5afeeaeaf75a0e309c038dc
Mohammed Salman <msal4@outlook.com>
2020-08-07 22:31:22 +0300

    Check admin email when registering only

--- api/src/modules/admin/admin/AdminRegisterInput.ts
+++ api/src/modules/admin/admin/AdminRegisterInput.ts
@@ -1,11 +1,18 @@
 import { InputType, Field } from "type-graphql";
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, IsEmail } from "class-validator";
 
 import { AdminLoginInput } from "@api/modules/admin/admin/AdminLoginInput";
+import { IsAdminEmailAlreadyExist } from "@api/modules/admin/admin/IsAdminEmailAlreadyExist";
 
 @InputType()
 export class AdminRegisterInput extends AdminLoginInput {
   @Field()
   @IsNotEmpty()
   name: string;
+
+  @Field()
+  @IsNotEmpty()
+  @IsEmail()
+  @IsAdminEmailAlreadyExist()
+  email: string;
 }
