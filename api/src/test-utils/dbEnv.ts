59b2ff144c81e60f3474077f966c75b553ff28e2
Mohammed Salman <msal4@outlook.com>
2020-08-08 20:24:05 +0300

    Add afterEach call to clean the database after each test

--- api/src/test-utils/dbEnv.ts
+++ api/src/test-utils/dbEnv.ts
@@ -1,9 +1,19 @@
-import { getConnection, createConnection } from "typeorm";
+import { getConnection, createConnection, getRepository } from "typeorm";
 
 import { ormconfig } from "@api/test-utils/ormconfig";
 
 beforeAll(async () => {
   await createConnection({ ...ormconfig, synchronize: false, dropSchema: false });
+});
+afterEach(async () => {
+  try {
+    // Clean database after each test
+    for (const entity of getConnection().entityMetadatas) {
+      await getRepository(entity.name).query(`TRUNCATE TABLE "${entity.tableName}" CASCADE;`);
+    }
+  } catch (err) {
+    console.log(`ERROR: Cleaning test database: ${err}`);
+  }
 });
 afterAll(async () => {
   await getConnection().close();
