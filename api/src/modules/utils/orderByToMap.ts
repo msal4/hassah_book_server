3a6bbce423679f344e1ea61f4b7b7c8e8095bede
Mohammed Salman <msal4@outlook.com>
2020-08-12 01:21:21 +0300

    Add prefixes to solve the field ambiguity issue with findManyToMany

--- api/src/modules/utils/orderByToMap.ts
+++ api/src/modules/utils/orderByToMap.ts
@@ -1,8 +1,8 @@
 import { OrderByMap, OrderType } from "@api/modules/shared/types/FilterArgs";
 
-export function orderByToMap<T>(order?: OrderByMap[]) {
+export function orderByToMap<T>(order?: OrderByMap[], ...prefixes: string[]) {
   return order?.reduce(
-    (acc, map) => ({ ...acc, [map.field]: map.order }),
+    (acc, map) => ({ ...acc, [`${prefixes.join("")}${map.field}`]: map.order }),
     {} as { [p in keyof T]: OrderType }
   );
 }
