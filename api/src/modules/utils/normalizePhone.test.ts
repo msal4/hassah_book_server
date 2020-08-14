e4083ccc4b7f63e42f4b08b3ee20837a6ca4e805
Mohammed Salman <msal4@outlook.com>
2020-08-14 04:58:08 +0300

    [fix] normalizePhone test

--- api/src/modules/utils/normalizePhone.test.ts
+++ api/src/modules/utils/normalizePhone.test.ts
@@ -5,14 +5,14 @@ import { normalizePhone } from "@api/modules/utils/normalizePhone";
 describe("Normalize phone", () => {
   it("normalize numbers starting with +96477", () => {
     const phone = faker.phone.phoneNumber("+96477########");
-    expect(normalizePhone(phone)).toBe(phone.substr(4));
+    expect(normalizePhone(phone)).toBe("+964" + phone.substr(4));
   });
   it("normalize numbers starting with 0096477", () => {
     const phone = faker.phone.phoneNumber("0096477########");
-    expect(normalizePhone(phone)).toBe(phone.substr(5));
+    expect(normalizePhone(phone)).toBe("+964" + phone.substr(5));
   });
   it("normalize numbers starting with 077", () => {
     const phone = faker.phone.phoneNumber("077########");
-    expect(normalizePhone(phone)).toBe(phone.substr(1));
+    expect(normalizePhone(phone)).toBe("+964" + phone.substr(1));
   });
 });
