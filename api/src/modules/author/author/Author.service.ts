da8f63817e11f7c630240c4a5ad571fb47bb49c5
Mohammed Salman <msal4@outlook.com>
2020-08-17 07:14:21 +0300

    [remove] unnecessary Entitiy field

--- api/src/modules/author/author/Author.service.ts
+++ api/src/modules/author/author/Author.service.ts
@@ -4,6 +4,4 @@ import { BaseService } from "@api/modules/shared/services/Base.service";
 import { Author } from "@api/entity/Author";
 
 @Service()
-export class AuthorService extends BaseService<Author> {
-  Entity = Author;
-}
+export class AuthorService extends BaseService<Author> {}
