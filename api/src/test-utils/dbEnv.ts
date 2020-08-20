40b70ac75a0b8f27df2e19356215eab62b41553f
Mohammed Salman <msal4@outlook.com>
2020-08-20 14:26:52 +0300

    [fix] move cleaning tables to afterEach

--- api/src/test-utils/dbEnv.ts
+++ api/src/test-utils/dbEnv.ts
@@ -6,7 +6,7 @@ beforeAll(async () => {
   await createConnection({ ...ormconfig, synchronize: false, dropSchema: false });
 });
 
-afterAll(async () => {
+afterEach(async () => {
   try {
     for (const entity of getConnection().entityMetadatas) {
       await getRepository(entity.name).query(`TRUNCATE TABLE "${entity.tableName}" CASCADE;`);
@@ -14,5 +14,8 @@ afterAll(async () => {
   } catch (err) {
     console.log(`ERROR: Cleaning test database: ${err}`);
   }
+});
+
+afterAll(async () => {
   await getConnection().close();
 });
