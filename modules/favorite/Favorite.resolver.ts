0c07d4b8c68559eca7b87a00d67748679d08b8b8
Mohammed Salman <msal4@outlook.com>
2021-04-24 18:49:21 +0300

    add favorites resolver

--- modules/favorite/Favorite.resolver.ts
+++ modules/favorite/Favorite.resolver.ts
@@ -1,12 +1,22 @@
-import { Resolver, Mutation, Arg, Ctx, Authorized } from "type-graphql";
+import { Resolver, Mutation, Arg, Ctx, Authorized, Root, Args, Query } from "type-graphql";
 
 import { FavoriteService } from "@api/modules/favorite/favorite/Favorite.service";
 import { RequestContext } from "@api/modules/types/RequestContext";
 import { Roles } from "@api/modules/utils/auth";
+import { PaginatedFavoriteResponse } from "@api/modules/types/PaginatedResponse";
+import { PAGINATED_RESPONSE_COMPLEXITY } from "@api/modules/constants/query";
+import { User } from "@api/entity/User";
+import { FilterArgs } from "@api/modules/types/FilterArgs";
 
 @Resolver()
 export class FavoriteResolver {
   constructor(private readonly favoriteService: FavoriteService) {}
+
+  @Authorized(Roles.User)
+  @Query(() => PaginatedFavoriteResponse, { complexity: PAGINATED_RESPONSE_COMPLEXITY })
+  favorites(@Root() user: User, @Args() args: FilterArgs): Promise<PaginatedFavoriteResponse> {
+    return this.favoriteService.findAll({ where: { user }, ...args });
+  }
 
   @Authorized(Roles.User)
   @Mutation(() => Boolean)
